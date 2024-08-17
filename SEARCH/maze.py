ecfd80523c0e07ba35cd1cce74a6e3e22dfd8330
DerickMghanga <derickmghanga3329@gmail.com>
2024-08-17 17:35:48 +0300

    knowledge

--- SEARCH/maze.py
+++ SEARCH/maze.py
@@ -19,10 +19,43 @@ class StackFrontier():
     def empty(self): # check if the frontier is empty
         return len(self.frontier) == 0
     
-    def remove(self, node):
-        if self.empty():
+    def remove(self):
+        if self.empty():  
             raise Exception("empty frontier")
         else:
             node == self.frontier[-1]
-            self.frontier = self.frontier[:-1]
+            self.frontier = self.frontier[:-1]  # last-in first-out approach(DEEP-FIRST SEARCH)
             return node
+        
+class QueueFrontier(StackFrontier):
+
+    def remove(self): 
+        if self.empty():
+            raise Exception('empty frontier')
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]  # first-in first-out approach(BREATH-FIRST SEARCH)
+            return node
+        
+class Maze():
+
+    def __init__(self, filename):
+
+        # Read file and set height and width of the maze
+        with open(filename) as f:
+            contents = f.read()
+
+        #validate start and goal
+        if contents.count("A") != 1:
+            raise Exception("Maze MUST have exactly one start point!")
+        if contents.count("B") != 1:
+            raise Exception("Maze MUST have exactly one goal!")
+        
+        #Determine height and width of maze
+        contents = contents.splitlines()
+        self.height = len(contents)
+        self.width = max(len(line) for line in contents)
+
+        #keep track of walls
+        self.walls = []
+        for i in range(self.height):
